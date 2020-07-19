589e119a81504dd362ba009899f5d5aa6ea5418e
JS GREWAL <jsgrewal1204@outlook.com>
2020-07-19 20:01:50 +0530

    @CrashRootA12 Establishing GitConnect

---
+++ example/easy_way_mysql_example.dart
@@ -0,0 +1,6 @@
+import 'package:easy_way_mysql/easy_way_mysql.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/easy_way_mysql.dart
@@ -0,0 +1,22 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library easy_way_mysql;
+
+import 'package:mysql1/mysql1.dart';
+
+void main() {}
+
+class EasyWay {
+ dynamic connection;
+  Future<MySqlConnection> createConnection(
+      String host, String user, String password, String database,
+      [int port = 3306]) async {
+    var settings = ConnectionSettings(
+        host: host, user: user, password: password, db: database, port: port);
+    connection = await MySqlConnection.connect(settings);
+    return connection;
+  }
+  
+}
+
