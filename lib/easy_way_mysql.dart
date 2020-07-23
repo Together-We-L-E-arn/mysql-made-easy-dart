57e9cf5ee0cceb8461fe2ac9d15d6f20807c420b
JS GREWAL <jsgrewal1204@outlook.com>
2020-07-24 00:53:43 +0530

    @CrashRootA12 selectQueryWithoutWhereClause Implemented

--- lib/easy_way_mysql.dart
+++ lib/easy_way_mysql.dart
@@ -6,18 +6,45 @@ library easy_way_mysql;
 import 'package:mysql1/mysql1.dart';
 
 void main() async {
-  
+  var x = MinifiedMySQLConnector();
+ await x.executeSelectQuery('x', columnAliases: m);
 }
 
+final emptyMap = <String, String>{};
+final m = {"A": "a"};
+
 class MinifiedMySQLConnector {
-   dynamic connection;
- 
-   Future<MySqlConnection> createConnection(
+  MySqlConnection connection;
+  Future<MySqlConnection> createConnection(
       String host, String user, String password, String database,
       [int port = 3306]) async {
     var settings = ConnectionSettings(
         host: host, user: user, password: password, db: database, port: port);
     connection = await MySqlConnection.connect(settings);
     return connection;
+  }
+
+  Future<Results> executeSelectQuery(String table,
+      {Map<String, String> columnAliases}) async {
+    var selectQueryString = _getSelectQueryString(columnAliases);
+    var query = 'select $selectQueryString from ${table}';
+    var results = await connection.query(query);
+    return results;
+  }
+
+  String _getSelectQueryString(Map<String, String> columnAliases) {
+    var selectQueryStr = '';
+    if (columnAliases.isEmpty) {
+      selectQueryStr = '*';
+    } else {
+      columnAliases.forEach((colName, aliasName) {
+        selectQueryStr += '$colName';
+        if (aliasName != '*') selectQueryStr += 'as $aliasName,';
+      });
+      if (selectQueryStr.endsWith(',')) {
+        selectQueryStr = selectQueryStr.substring(0, selectQueryStr.length - 1);
+      }
+    }
+    return selectQueryStr;
   }
 }
---
+++ lib/test/testing_concepts.dart
@@ -0,0 +1,8 @@
+library easy_way_mysql;
+
+void main() {
+  var x = {'x': '1', 'y': '2'};
+  x.forEach((key, value) {
+    print(key+' ' +value);
+  });
+}
